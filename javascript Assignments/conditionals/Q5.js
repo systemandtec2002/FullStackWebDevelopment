e0eae2d840bc694a13e38a846e5acccd0f1a1610
Mayuri ashok modi <mayurimodi368@gmail.com>
2024-08-15 18:37:49 +0530

    assignments of conditional

---
+++ javascript Assignments/conditionals/Q5.js
@@ -0,0 +1,27 @@
+// Q5. Use the while loop to print n9mbers from 1 to 25 in ascending and descending order.
+
+// let num = 1;
+
+// while (num <= 25) {
+//   console.log(num);
+//   num++;
+// }
+
+
+// let num = 1;
+// while(num <= 25){
+// console.log(num);
+// num++;
+// }
+
+
+let num = 1;
+while(num >= 25){
+console.log(num);
+num++;
+}
+
+for (let i = 25; i >= 1; i--) {
+    console.log(i);
+  }
+  
