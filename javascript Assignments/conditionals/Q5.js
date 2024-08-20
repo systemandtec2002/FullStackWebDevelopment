f23ef0fb433fd86c7fe22d4c3507e56896e0780c
Mayuri ashok modi <mayurimodi368@gmail.com>
2024-08-20 23:10:37 +0530

    chages done

--- javascript Assignments/conditionals/Q5.js
+++ javascript Assignments/conditionals/Q5.js
@@ -1,27 +1,13 @@
 // Q5. Use the while loop to print n9mbers from 1 to 25 in ascending and descending order.
 
-// let num = 1;
-
-// while (num <= 25) {
-//   console.log(num);
-//   num++;
-// }
-
-
-// let num = 1;
-// while(num <= 25){
-// console.log(num);
-// num++;
-// }
-
-
-let num = 1;
-while(num >= 25){
-console.log(num);
-num++;
+// ascending order
+while (num1 <= num2) {
+console.log(num1);
+num1++;
 }
 
-for (let i = 25; i >= 1; i--) {
-    console.log(i);
-  }
-  
+// descending order
+while (num2 >= 1) {
+console.log(num2);
+num2--;
+}
---
+++ javascript Assignments/function in javascript/Q1.js
@@ -0,0 +1,9 @@
+// Q1. Create an arrow function called square that takes a number as an argument and returns its square. Use the arrow function to calculate the square of a given number and display the result.
+
+const square = (num) => num * num;
+
+const numberToSquare = 5;
+const result = square(numberToSquare);
+
+console.log("The square of", numberToSquare, "is", result);
+
