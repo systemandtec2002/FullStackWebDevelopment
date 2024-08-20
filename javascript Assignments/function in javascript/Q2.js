8a7b1a2796fd05eab6752ced181337e3ee0f19fc
Mayuri ashok modi <mayurimodi368@gmail.com>
2024-08-20 23:48:03 +0530

    assginments of pw

---
+++ javascript Assignments/function in javascript/Q2.js
@@ -0,0 +1,16 @@
+// Q2. Create a JavaScript function called generateGreeting that takes a name as an argument and returns a
+// personalized greeting message. Use this function to greet three different people.
+
+function generateGreeting(name) {
+    return "Hello," + name + "! How are you today?";
+  }
+  
+  const greeting1 = generateGreeting("Mayuri");
+  const greeting2 = generateGreeting("Megha");
+  const greeting3 = generateGreeting("Omkar");
+  
+  console.log(greeting1);
+  console.log(greeting2);
+  console.log(greeting3);
+
+
